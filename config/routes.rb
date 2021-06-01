afe0de869ee5e9d63a2d178da4f32b17e54804ff
masaya-kawamura <lucky.number0128@gmail.com>
2021-06-01 18:17:55 +0000

    [終了]Bookers2にフォロー&フォロワー機能を追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,18 @@ Rails.application.routes.draw do
   root 'homes#top'
   get 'home/about' => 'homes#about'
   devise_for :users
-  resources :users,only: [:show,:index,:edit,:update]
+
+  resources :users,only: [:show,:index,:edit,:update] do
+    member do
+      # フォローとフォロワー一覧ページのルーティング
+      get :following, :followers
+    end
+    # フォロー&アンフォローのためのルーティング
+    resource :relationships, only: [:create, :destroy]
+  end
+
+
+
   resources :books do
     resource :favorites, only: [:create, :destroy]
     resources :comments, only: [:create, :destroy]
